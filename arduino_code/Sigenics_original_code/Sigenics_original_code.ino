ec7126038f69aaf3b55999535231a0fbccb17c5e
radhen <radhen.17@gmail.com>
2019-02-12 14:17:34 -0700

    Testing PID control for hand

--- arduino_code/Sigenics_original_code/Sigenics_original_code.ino
+++ arduino_code/Sigenics_original_code/Sigenics_original_code.ino
@@ -14,113 +14,138 @@
 #define ESCAPE 0x7D
 #define HDLC 0x7E
 #define SCAN_I2C 0x2C  //break hdlc to tell the arduino to scan the i2c bus
-                      //and report back any ACK'd addresses
-                      //return format goes:
-                      
-                      //|| # of addresses || device 1 address || device 2 address || ... || device x address ||
+//and report back any ACK'd addresses
+//return format goes:
+
+//|| # of addresses || device 1 address || device 2 address || ... || device x address ||
 
 
 byte inBuf[16];
 byte i2cBuf[16];
-byte outCount=0;
-byte state=fNONE;
-bool newCommand=false, toggle=false;
+byte outCount = 0;
+byte state = fNONE;
+bool newCommand = false, toggle = false, debugFlag = true;
 byte inByte;
 
-void setup(void){
+byte i2cBuf_test[16];
+byte i2cBuf_break[16];
+
+
+
+void setup(void) {
   // Serial port
   Serial.begin(115200);
 
   //i2c
   Wire.begin();
-  newCommand=false;
+  newCommand = false;
+
+  // hex to close the finger
+//    i2cBuf_test[0] = 0x06;
+//    i2cBuf_test[1] = 0x0C;
+//    i2cBuf_test[2] = 0x80;
+//    i2cBuf_test[3] = 0x30;
+
+  // hex to open the finger
+  i2cBuf_test[0] = 0x06;
+  i2cBuf_test[1] = 0x0C;
+  i2cBuf_test[2] = 0xC0;
+  i2cBuf_test[3] = 0x40;
+
+  // hex to break the finger
+  i2cBuf_break[0] = 0x06;
+  i2cBuf_break[1] = 0x0C;
+  i2cBuf_break[2] = 0x03;
+  i2cBuf_break[3] = 0x00;
+
+  //  outCount = 4;
 }
-void toggleLED(void){
-    toggle = !toggle;
-      if (toggle){
-        digitalWrite(13,HIGH);
-      }
-      else{
-        digitalWrite(13,LOW);
-      }
+void toggleLED(void) {
+  toggle = !toggle;
+  if (toggle) {
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
 }
 
-void scan_i2c(void){
+void scan_i2c(void) {
   byte ct, res, devCt;
-  devCt=res=0;
+  devCt = res = 0;
   byte addList[72];
-  for (ct=1;ct<0x48;ct++){
+  for (ct = 1; ct < 0x48; ct++) {
     Wire.beginTransmission(ct);
     Wire.write(0x1);
     res = Wire.endTransmission();
-    if(!res){
-      addList[devCt]=ct;
-      ++devCt;  
+    if (!res) {
+      addList[devCt] = ct;
+      ++devCt;
     }
   }
   Serial.print(devCt);
-  for(ct=0;ct<devCt;ct++){
+  for (ct = 0; ct < devCt; ct++) {
     Serial.print(addList[ct]);
-  } 
+  }
 }
 
 bool lookForData()
 {
   if (Serial.available() > 0)
   {
-    while(Serial.available()){
+    while (Serial.available()) {
       inByte = Serial.read();
-      
-      switch(state){
+
+      switch (state) {
         case fNONE:
-          if(inByte==HDLC){
-            state=fSTART; 
-            outCount=0;
+          if (inByte == HDLC) {
+            state = fSTART;
+            outCount = 0;
           }
-          else if(inByte==0x2C){  //break HDLC in this way to scan I2C bus
-                                  //and report addresses of present devices
-                                  //comment out the line 'scan_i2c()' to disable
-                                  //and return to pure HDLC
-            outCount=0;
-            scan_i2c(); 
+          else if (inByte == 0x2C) { //break HDLC in this way to scan I2C bus
+            //and report addresses of present devices
+            //comment out the line 'scan_i2c()' to disable
+            //and return to pure HDLC
+            outCount = 0;
+            scan_i2c();
           }
           break;
-    
+
         case fSTART:
-          if(inByte==HDLC){
-            state=fNONE;
-            newCommand=true;
+          if (inByte == HDLC) {
+            state = fNONE;
+            newCommand = true;
           }
-          else if(inByte==ESCAPE){
-            state=fESCAPE;
+          else if (inByte == ESCAPE) {
+            state = fESCAPE;
           }
-          else{
-            i2cBuf[outCount]=inByte;
+          else {
+            i2cBuf[outCount] = inByte;
             ++outCount;
-             toggleLED();
+            toggleLED();
           }
           break;
-    
+
         case fESCAPE:
-          if(inByte==0x5E){
-           i2cBuf[outCount]=0x7E;
-           ++outCount;    
+          if (inByte == 0x5E) {
+            i2cBuf[outCount] = 0x7E;
+            ++outCount;
           }
-          else if(inByte==0x5D){
-           i2cBuf[outCount]=0x7D;
-           ++outCount;
-          } 
-          state=fSTART;
+          else if (inByte == 0x5D) {
+            i2cBuf[outCount] = 0x7D;
+            ++outCount;
+          }
+          state = fSTART;
           break;
-         
+
         default:
-        
+
           break;
-        
+
       }
     }
     //uncomment the line below to echo back to debug
-//    Serial.write(inByte); //echo back       
+    //    Serial.write(inByte); //echo back
     return true;  //got data
   }
   else
@@ -131,34 +156,60 @@ bool lookForData()
 
 
 
-void obey(){
+void obey() {
   byte inByte;
-  if(i2cBuf[0]&0x1){
-      //it's a read command
-      Wire.requestFrom(i2cBuf[0]>>1,outCount-1);
-      while(Wire.available()){
-       inByte=Wire.read();
-       Serial.print(inByte); 
-      }
+  if (i2cBuf[0] & 0x1) {
+    //it's a read command
+    Wire.requestFrom(i2cBuf[0] >> 1, outCount - 1);
+    while (Wire.available()) {
+      inByte = Wire.read();
+      Serial.println(inByte);
     }
-    else{
-      //it's a write command
-      Wire.beginTransmission(i2cBuf[0]>>1);
-      Wire.write(&i2cBuf[1], outCount-1);
-      Wire.endTransmission(); 
+  }
+  else {
+    //it's a write command
+    //      Serial.println(i2cBuf[1]);
+    Wire.beginTransmission(i2cBuf[0] >> 1);
+    Wire.write(&i2cBuf[1], outCount - 1);
+    Wire.endTransmission();
+  }
+}
+
+void move_finger() {
+
+  for (int i = 0; i < 4; i++) {
+    //    Serial.println(i2cBuf_test[i]);
+    Wire.beginTransmission(i2cBuf_test[0] >> 1);
+    Wire.write(&i2cBuf_test[1], outCount - 1);
+    Wire.endTransmission();
+  }
+
+  Serial.println("Done moving.");
+
+  for (int i = 0; i < 4; i++) {
+    //    Serial.println(i2cBuf_test[i]); 
+    Wire.beginTransmission(i2cBuf_break[0] >> 1);
+    Wire.write(&i2cBuf_break[1], outCount - 1);
+    Wire.endTransmission();
+  }
+
+  Serial.println("Applying breaks!");
+
+}
+
+void loop() {
+
+    lookForData();
+    if(newCommand==true){
+      obey();
+      newCommand=false;
     }
 
-   
+//    if(debugFlag==true){
+//      move_finger();
+//      debugFlag=false;
+//    }
+//    Serial.println("In da loop");
+
+
 }
-void loop(){
-  lookForData();
-  if(newCommand==true){
-    obey();
-    newCommand=false;
-  }
-  
-}
-  
-  
-  
-  
--- bebionicHand/motorcontrol.py
+++ bebionicHand/motorcontrol.py
@@ -1,14 +1,30 @@
 import serial
 import struct
 import numpy as np
+import time
 
 
 
 PORT = '/dev/ttyACM0'
 BAUDRATE = 115200
-NUM_P_BOARDS = 2
+NUM_P_BOARDS = 1
 
-SER = serial.Serial(PORT, BAUDRATE, timeout=0.1)
+'''
+HEX commands are send to the controller board. 
+The CLOSE command for e.g. starts and ends with a 
+terminator HEX character i.e x7E. x00 and x01
+are for write and read resp. (a bit long). This is followed by 
+a 7-bit address of the PBoard. x06 for has a 7-bit add. i.e 3 (000 0011)
+and x01 (one bit) for write command, resulting in x06 (0000 0110). 
+x0C is for setting the PWM value also called the command code and the rest two are payload
+bytes. Ref. PBoard manual for more details on this.  
+'''
+
+CLOSE = "\x7E\x08\x0C\x80\x40\x7E"
+OPEN = "\x7E\x06\x0C\xC0\x40\x7E"
+BREAK = "\x7E\x08\x0C\x03\x00\x7E"
+
+SER = serial.Serial(PORT, BAUDRATE, timeout=0.1, write_timeout=3)
 print(SER.isOpen())
 
 def get_addresses():
@@ -16,14 +32,58 @@ def get_addresses():
         try:
             # send 0x2C hex to serial to read board addresses
             SER.write(b"\x2C")
-            dataIn = SER.read_until(size=NUM_P_BOARDS)
-            if len(dataIn)==NUM_P_BOARDS:
+            dataIn = SER.read_until(size=NUM_P_BOARDS+1)
+            if len(dataIn)==NUM_P_BOARDS+1:
                 return dataIn
         except serial.SerialException as e:
             print ('There is no new data from serial port')
 
-# s = get_addresses()
-# print s
+s = get_addresses()
+print "Board address(es): "+str(s[1:])
 
-while 1:
-    SER.write("\x7E \x04 \x01 \x01 \x7D")
+
+#########################################
+#### Emergency break if serial clogs ####
+#########################################
+# while 1:
+#     SER.write(BREAK)
+
+
+# SER.flushOutput()
+# SER.flushInput()
+
+SER.write(CLOSE)
+time.sleep(3)
+# SER.write(OPEN)
+# time.sleep(2)
+# SER.write(BREAK)
+# time.sleep(2)
+
+
+
+SER.write("\x7E\x08\x08\x01\x7E") # Set Position Count = 1
+time.sleep(2)
+print "Set Position Count = 1"
+# SER.write("\x7E\x06\x0C\x80\x7E") # Set PWM
+# time.sleep(4)
+# print "Set PWM"
+SER.write("\x7E\x08\x80\x80\x7E") # Set Enable PID
+time.sleep(2)
+print "Set Enable PID"
+SER.write("\x7E\x08\x81\x03\x00\x00\x00\x7E") # Set Kp=3
+time.sleep(2)
+print "Set Kp=3"
+SER.write("\x7E\x08\x82\x3f\x00\x00\x00\x7E") # Set Ki=0.5 or 0x3f000000
+time.sleep(2)
+print "Set Ki=0.5 or 0x3f000000"
+SER.write("\x7E\x08\x83\x00\x00\x00\x00\x7E") # Set Kd=0
+time.sleep(2)
+print "Set Kd=0"
+SER.write("\x7E\x08\x84\x27\x10\x7E") # Set Target position = 100
+time.sleep(3)
+print "Set Target position = 100"
+SER.write(BREAK)
+time.sleep(2)
+
+print ("ANYTHING HAPPENED?")
+
