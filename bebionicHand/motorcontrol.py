11a0c96a1e739404172c594b0f406808c3482e67
radhen <radhen.17@gmail.com>
2019-02-15 13:57:51 -0700

    PID working

--- bebionicHand/motorcontrol.py
+++ bebionicHand/motorcontrol.py
@@ -20,15 +20,15 @@ x0C is for setting the PWM value also called the command code and the rest two a
 bytes. Ref. PBoard manual for more details on this.  
 '''
 
-CLOSE = "\x7E\x08\x0C\x80\x20\x7E"
-OPEN = "\x7E\x08\x0C\xC0\x10\x7E"
-BREAK = "\x7E\x08\x0C\x03\x00\x7E"
+CLOSE = "\x7E\x06\x0C\x80\x30\x7E"
+OPEN = "\x7E\x06\x0C\xC0\x30\x7E"
+BREAK = "\x7E\x06\x0C\x03\x00\x7E"
 
 
 
 def make_serial_connection(port, baudrate):
     try:
-        ser = serial.Serial(port, baudrate, timeout=1)  # This must match the port selected in the Arduino IDE
+        ser = serial.Serial(port, baudrate, timeout=0.1)  # This must match the port selected in the Arduino IDE
         print "Connected to port:", ser.name
         return ser
     except Exception as ex:
@@ -63,10 +63,10 @@ def get_addresses(ser):
 
 
 def read_pboards(ser):
-    ser.write("\x7E\x08\x01\x02\x7E") # SetReadTarget
-    time.sleep(0.5)
+    ser.write("\x7E\x08\x01\x06\x7E") # SetReadTarget
+    time.sleep(1)
     ser.write("\x7E\x09\x00\x00\x7E") # Sent read command to specific Pboard add.
-    time.sleep(0.5)
+    time.sleep(1)
     get_all_bytes_from_connection(ser)
 
 
@@ -85,8 +85,8 @@ get_addresses(ser)
 #########################################
 #### Debug: open and close finger ####
 #########################################
-ser.write(CLOSE)
-time.sleep(1)
+# ser.write(CLOSE)
+# time.sleep(1)
 # ser.write(OPEN)
 # time.sleep(1)
 # ser.write(BREAK)
@@ -94,32 +94,29 @@ time.sleep(1)
 
 
 
-ser.write("\x7E\x08\x08\x03\xE8\x7E") # Set Position Count = 1000
-print "Set Position Count = 1000"
+
+ser.write(b"\x7E\x06\x08\x0B\xB8\x7E") # Set Position Count = 3500
+print "Set Position Count = 3000"
 time.sleep(2)
 
-# ser.write("\x7E\x08\x0C\x00\x7E") # Set Coast
-# print "Set Coast"
-# time.sleep(2)
+ser.write(b"\x7E\x06\x81\x40A00000\x7E") # Set Kp
+print "Set Kp = 5.0"
+time.sleep(2)
 
-ser.write("\x7E\x08\x80\x80\x7E") # Set Enable PID
+ser.write(b"\x7E\x06\x82\x3c23d70a\x7E") # Set Ki
+print "Set Ki = 0.01"
+time.sleep(2)
+
+ser.write(b"\x7E\x06\x83\x3e4ccccd\x7E") # Set Kd=0
+print "Set Kd = 0.5"
+time.sleep(2)
+
+ser.write(b"\x7E\x06\x84\x00\x64\x7E") # Set Target position = 4000
+print "Set Target position = 100"
+time.sleep(2)
+
+ser.write(b"\x7E\x06\x80\x80\x7E") # Set Enable PID
 print "Set Enable PID"
-time.sleep(2)
-
-ser.write("\x7E\x08\x81\x10\x7E") # Set Kp=10
-print "Set Kp = 10"
-time.sleep(2)
-
-ser.write("\x7E\x08\x82\x00\x7E") # Set Ki=0.5 or 0x3f000000
-print "Set Ki = 0"
-time.sleep(2)
-
-ser.write("\x7E\x08\x83\x00\x7E") # Set Kd=0
-print "Set Kd = 0"
-time.sleep(2)
-
-ser.write("\x7E\x08\x84\x13\x88\x7E") # Set Target position = 5000
-print "Set Target position = 5000"
 time.sleep(2)
 
 ser.write(BREAK)
