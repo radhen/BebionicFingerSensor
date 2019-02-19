ac91cbe5f97ad91985560fe953561b82a82b0270
radhen <radhen.17@gmail.com>
2019-02-18 23:14:34 -0700

    nn c some prog

--- nn_c/modelWeg_textfile.py
+++ nn_c/modelWeg_textfile.py
@@ -7,13 +7,13 @@ from keras.models import load_model
 from keras.models import model_from_json
 #np.set_printoptions(threshold=np.nan)
 
-f= h5py.File('nn_motion3_reg_weights.best.hdf5', 'r')
+f= h5py.File('weights.best.hdf5', 'r')
 # model= load_model('weights.best.hdf5')
-json_file = open('model.nn_motion3_reg.json', 'r')
+json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 model = model_from_json(loaded_model_json)
-textFile= open("wr_layer_{}.txt".format(sys.argv[1]), "w")
+textFile= open("wr_lyer_{}.txt".format(sys.argv[1]), "w")
 
 
 layer= int(sys.argv[1])
@@ -48,15 +48,15 @@ strBiases= strBiases.replace(']', '}')
 print()
 print(strBiases)
 
-# print(model.layers[0])
-# print("Keys: %s" % f.keys())
-# a_group_key= list(f.keys())[0]
-#
+print(model.layers[0])
+print("Keys: %s" % f.keys())
+a_group_key= list(f.keys())[0]
+
 # test= np.ones((1, 50, 2))
 # layer_outs=functor([test, layer])
 # print(layer_outs)
-#
-#
-# data= list(f[a_group_key])
-# print(data)
+
+
+data= list(f[a_group_key])
+print(data)
 
